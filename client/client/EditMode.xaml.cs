719f8ede82069243a3823edb6cc8b09c77975e87
Warh40k <kommunist.bear@gmail.com>
2022-04-05 17:20:06 +0300

    Add working navigation buttons

--- client/client/EditMode.xaml.cs
+++ client/client/EditMode.xaml.cs
@@ -14,21 +14,33 @@ namespace client
         public DbAccess db;
         public DataTable table;
         List<string> columns;
+        public int currentId;
 
-        public EditMode(DbAccess db, DataTable table)
+        public EditMode(DbAccess db, DataTable table, int currentId = 0)
         {
             InitializeComponent();
+
             this.db = db;
             this.table = table;
-            DataRowCollection row = table.Rows;
+            this.currentId = currentId;
+            ChangeId(currentId);
+
+        }
+        void ChangeId(int id)
+        {
+            wrapPanel.Children.Clear();
+            DataRowCollection rows = table.Rows;
 
             columns = db.GetColumns(table);
-            int i = 0;
+            note_label.Content = rows[id][0];
 
-            foreach(string column in columns)
+            wrapPanel.Children.Add(new Label() { Content = columns[0], Margin = new Thickness(5), MaxWidth = 120 });
+            wrapPanel.Children.Add(new TextBox() { Text = rows[id][0].ToString(), Margin = new Thickness(5), MaxWidth = 120, IsReadOnly = true });
+
+            for (int i = 1; i < columns.Count; i++)
             {
-                wrapPanel.Children.Add(new Label() { Content = column, Margin = new Thickness(5), MaxWidth = 120});
-                wrapPanel.Children.Add(new TextBox() {Text = row[0][i++].ToString(), Margin = new Thickness(5), MaxWidth = 120});
+                wrapPanel.Children.Add(new Label() { Content = columns[i], Margin = new Thickness(5), MaxWidth = 120 });
+                wrapPanel.Children.Add(new TextBox() { Text = rows[id][i++].ToString(), Margin = new Thickness(5), MaxWidth = 120 });
             }
         }
 
@@ -40,6 +52,16 @@ namespace client
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void forward_button_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeId(++currentId);
+        }
+
+        private void backward_button_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeId(--currentId);
         }
     }
 }
